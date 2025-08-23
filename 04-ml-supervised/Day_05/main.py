e3ccde9958435705e3e9232341be9fba302a0a2a
codebydevfer <fernandopeandrade@outlook.com>
2025-08-22 22:39:47 -0400

    Day 11

--- 04-ml-supervised/Day_05/main.py
+++ 04-ml-supervised/Day_05/main.py
@@ -2,7 +2,14 @@
 
 #Train/Test
 
+#1 - Import all necessary libraries
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 
 #cross-validation (KFold)
 
